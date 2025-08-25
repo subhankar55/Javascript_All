cc82f6d48d3ef3242b277ade4410653acd7a3e94
Subhankar <maitisubhankar55@gmail.com>
2025-08-25 11:51:00 +0530

    update all

--- BackEnd/Express/app.js
+++ BackEnd/Express/app.js
@@ -1,20 +1,24 @@
 const express = require('express');
-
+const path = require("path");
 const app = express();
 
 const port = 80;
-app.get("/",(req,res)=>{
-    res.status(200).send("This is home page of my first express app.")
-});
-app.get("/about",(req,res)=>{
-    res.send("This is about page of my first express app.")
-});
-app.post("/about",(req,res)=>{
-    res.send("This is post request about page of my first express app.")
-});
-app.post("/this",(req,res)=>{
-    res.status(400).send("Page not found.")
-});
+// EXPRESS SPECIFIC STUFF
+app.use('/static',express.static('static')); // For serving static files
+
+// PUG SPECIFIC STUFF
+app.set('view engine','pug'); // set the template engine as pug
+app.set('views',path.join(__dirname,'views')); // set the views directory
+
+// ENDPOINTS
+app.get('/',(req,res)=>{
+    const con = 'This is the best content so far u have used in the internet.'
+    const params = {'title': 'PUBG is the best game', "content": con};
+    res.status(200).render('index.pug',params);
+})
+
+
+// START THE SERVER
 app.listen(port,()=>{
     console.log(`This application started successfully on ${port}`)
 });
